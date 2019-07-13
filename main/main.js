970655325e309cbdbcc9171bfeddaf8d686174f4
DARYL HUANG (ITA-ISDC-ISD-OOCLL/ZHA) <HUANGDA7@OOCL.COM>
2019-07-13 17:16:49 +0800

    refactor:reduce code amount by using ES6 grammer

--- main/main.js
+++ main/main.js
@@ -3,31 +3,10 @@
 //TODO: 请在该文件中实现练习要求并删除此注释
 const isBarcodesValid=(barcodes)=>{//传进来的实际是tags
     const itemList=loadAllItems();
-    let count=0;
-    for(let i=0;i<barcodes.length;i++){
-        if(barcodes[i].length>10){
-            let str=barcodes[i].split("-")[0];
-            for(let j=0;j<itemList.length;j++){
-                if(str==itemList[j].barcode){
-                    count++;
-                    break;
-                }
-            }
-            barcodes[i].split("-")[0]
-        }
-        else{
-            for(let j=0;j<itemList.length;j++){
-                if(barcodes[i]==itemList[j].barcode){
-                    count++;
-                    break;
-                }
-            }
-        }
-    }
-    if(count==barcodes.length)
-    return true;
-    else
-    return false;
+    let barcodeInItemList=itemList.map(item=>item.barcode);
+    let newBarcodes=barcodes.map(item=>item.length<=10?item:item.substring(0,10));
+    let sameElementInBarcodesAndInBarcodeInItemList=newBarcodes.filter(item=>barcodeInItemList.includes(item));
+    return (sameElementInBarcodesAndInBarcodeInItemList.length==barcodes.length)?true:false;
 }
 
 
