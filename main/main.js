88d45330c689658d40ff5567d36c53ae7aa2d184
DARYL HUANG (ITA-ISDC-ISD-OOCLL/ZHA) <HUANGDA7@OOCL.COM>
2019-07-14 17:15:27 +0800

    feature:add function printReceipt
    make it able to pass the test in main-test.js

--- main/main.js
+++ main/main.js
@@ -10,18 +10,31 @@ const isBarcodesValid=(barcodes)=>{//传进来的实际是tags
     return validFlag;
 }
 
+const getDetailBarcodesInfo=(barcodes)=>{
+    if(!isBarcodesValid(barcodes))
+    return "The barcodes are wrong!"
+    else{
+    let detailBarcodesInfo=barcodes.reduce(callback,[]);
+    return detailBarcodesInfo;
+    }
+}
+
 const callback=(previousValue,currentValue)=>{
     const itemList=loadAllItems();
     if(previousValue.length==0){
         let obj={};
         if(currentValue.length<=10){
             obj.barcode=currentValue;
+            obj.name=itemList[itemList.map(item1=>item1.barcode).indexOf(currentValue)].name;
+            obj.unit=itemList[itemList.map(item1=>item1.barcode).indexOf(currentValue)].unit;
             obj.number=1;
             obj.unitPrice=itemList[itemList.map(item1=>item1.barcode).indexOf(currentValue)].price;
         }
         else{
             let splitBarcode=currentValue.split("-");
             obj.barcode=splitBarcode[0];
+            obj.name=itemList[itemList.map(item1=>item1.barcode).indexOf(splitBarcode[0])].name;
+            obj.unit=itemList[itemList.map(item1=>item1.barcode).indexOf(splitBarcode[0])].unit;
             obj.number=parseFloat(splitBarcode[1]);
             obj.unitPrice=itemList[itemList.map(item1=>item1.barcode).indexOf(splitBarcode[0])].price;
         }
@@ -35,6 +48,8 @@ const callback=(previousValue,currentValue)=>{
             else{
                 let obj={};
                 obj.barcode=currentValue;
+                obj.name=itemList[itemList.map(item1=>item1.barcode).indexOf(currentValue)].name;
+                obj.unit=itemList[itemList.map(item1=>item1.barcode).indexOf(currentValue)].unit;
                 obj.number=1;
                 obj.unitPrice=itemList[itemList.map(item1=>item1.barcode).indexOf(currentValue)].price;
                 previousValue.push(obj);
@@ -48,6 +63,8 @@ const callback=(previousValue,currentValue)=>{
             else{
                 let obj={};
                 obj.barcode=splitBarcode1[0];
+                obj.name=itemList[itemList.map(item1=>item1.barcode).indexOf(splitBarcode1[0])].name;
+                obj.unit=itemList[itemList.map(item1=>item1.barcode).indexOf(splitBarcode1[0])].unit;
                 obj.number=parseFloat(splitBarcode1[1]);
                 obj.unitPrice=itemList[itemList.map(item1=>item1.barcode).indexOf(splitBarcode1[0])].price;
                 previousValue.push(obj);
@@ -57,15 +74,11 @@ const callback=(previousValue,currentValue)=>{
     }
     return previousValue;
 }
-const getDetailBarcodesInfo=(barcodes)=>{
-    if(!isBarcodesValid(barcodes))
-    return "The barcodes are wrong!"
-    else{
-    let detailBarcodesInfo=barcodes.reduce(callback,[]);
-    return detailBarcodesInfo;
-    }
-}
 
+const calculateSubtotal=(detailBarcodesInfo)=>{
+    let receiptInformation=detailBarcodesInfo.reduce(callback1,detailBarcodesInfo);
+    return receiptInformation;
+}
 
 const callback1=(previousValue,currentValue)=>{
     let promotionBarcodeslist=loadPromotions()[0].barcodes;
@@ -79,10 +92,6 @@ const callback1=(previousValue,currentValue)=>{
     }
     return previousValue;
 }
-const calculateSubtotal=(detailBarcodesInfo)=>{
-    let receiptInformation=detailBarcodesInfo.reduce(callback1,detailBarcodesInfo);
-    return receiptInformation;
-}
 
 const calculateSumOfSubtotal=(receiptInformation)=>{
     let sumOfSubtotal=receiptInformation.reduce((sum,currentValue)=>sum+currentValue.subtotal,0);
@@ -94,6 +103,13 @@ const getTotalPromotionPrice=(receiptInformation)=>{
     return totalPromotionPrice;
 }
 
+const printReceipt=(receiptInformation,sumOfSubtotal,totalPromotionPrice)=>{
+    let receipt=`***<没钱赚商店>收据***\n`;
+    let detailInfo=receiptInformation.map(item=>`名称：${item.name}，数量：${item.number}${item.unit}，单价：${item.unitPrice}(元)，小计：${item.subtotal}(元)`);
+    receipt+=detailInfo.reduce((previousValue,currentValue)=>previousValue+currentValue+'\n','');
+    receipt+=`----------------------\n总计：${sumOfSubtotal}(元)\n节省：${totalPromotionPrice}(元)\n**********************`;
+    return receipt;
+}
 
 
 
--- test/main-test.js
+++ test/main-test.js
@@ -25,18 +25,28 @@ describe('pos', () => {
     ];
 const detailBarcodesInfo=getDetailBarcodesInfo(tags);
 const receiptInformation=calculateSubtotal(getDetailBarcodesInfo(tags));
+const sumOfSubtotal=calculateSumOfSubtotal(receiptInformation);
+const totalPromotionPrice=getTotalPromotionPrice(receiptInformation);
+const receipt=`***<没钱赚商店>收据***
+名称：雪碧，数量：5瓶，单价：3(元)，小计：9(元)
+名称：荔枝，数量：2.5斤，单价：15(元)，小计：37.5(元)
+名称：方便面，数量：3袋，单价：4.5(元)，小计：9(元)
+----------------------
+总计：55.5(元)
+节省：10.5(元)
+**********************`;
 
 //     spyOn(console, 'log');
 
 //     printReceipt(tags);
 
 //     const expectText = `***<没钱赚商店>收据***
-// 名称：雪碧，数量：5瓶，单价：3.00(元)，小计：12.00(元)
-// 名称：荔枝，数量：2.5斤，单价：15.00(元)，小计：37.50(元)
-// 名称：方便面，数量：3袋，单价：4.50(元)，小计：9.00(元)
+// 名称：雪碧，数量：5瓶，单价：3(元)，小计：9(元)
+// 名称：荔枝，数量：2.5斤，单价：15(元)，小计：37.5(元)
+// 名称：方便面，数量：3袋，单价：4.5(元)，小计：9(元)
 // ----------------------
-// 总计：58.50(元)
-// 节省：7.50(元)
+// 总计：55.5(元)
+// 节省：10.5(元)
 // **********************`;
 
 //     expect(console.log).toHaveBeenCalledWith(expectText);
@@ -50,14 +60,14 @@ it('return false when some elements in barcodes are wrong', () => {
 })
 
 it('return detailBarcodesInfo when  call getDetailBarcodesInfo given barcodes', () => {
-  expect( getDetailBarcodesInfo(tags)).toEqual([{barcode: "ITEM000001",number: 5,unitPrice: 3},{barcode: "ITEM000003",number: 2.5,unitPrice: 15},{barcode: "ITEM000005",number: 3,unitPrice: 4.5}]);
+  expect( getDetailBarcodesInfo(tags)).toEqual([{barcode: "ITEM000001",name: "雪碧",unit: "瓶",number: 5,unitPrice: 3},{barcode: "ITEM000003",name: "荔枝",unit: "斤",number: 2.5,unitPrice: 15},{barcode: "ITEM000005",name: "方便面",unit: "袋",number: 3,unitPrice: 4.5}]);
 })
 it('return "The barcodes are wrong!" when call getDetailBarcodesInfo given wrong barcodes', () => {
   expect( getDetailBarcodesInfo(tags1)).toBe("The barcodes are wrong!");
 })
 
 it('return receiptInformation when call calculateSubtotal given detailBarcodesInfo', () => {
-  expect( calculateSubtotal(detailBarcodesInfo)).toEqual([{barcode: "ITEM000001",number: 5,unitPrice: 3,subtotal: 9},{barcode: "ITEM000003",number: 2.5,unitPrice: 15,subtotal: 37.5},{barcode: "ITEM000005",number: 3,unitPrice: 4.5,subtotal: 9}]);
+  expect( calculateSubtotal(detailBarcodesInfo)).toEqual([{barcode: "ITEM000001",name: "雪碧",unit: "瓶",number: 5,unitPrice: 3,subtotal: 9},{barcode: "ITEM000003",name: "荔枝",unit: "斤",number: 2.5,unitPrice: 15,subtotal: 37.5},{barcode: "ITEM000005",name: "方便面",unit: "袋",number: 3,unitPrice: 4.5,subtotal: 9}]);
 })
 
 it('return sumOfSubtotal when call calculateSumOfSubtotal given receiptInformation', () => {
@@ -68,6 +78,9 @@ it('return totalPromotionPrice when call getTotalPromotionPrice given receiptInf
   expect( getTotalPromotionPrice(receiptInformation)).toEqual(10.5);
 })
 
+it('return receipt when call printReceipt given receiptInformation,sumOfSubtotal and totalPromotionPrice', () => {
+  expect( printReceipt(receiptInformation,sumOfSubtotal,totalPromotionPrice)).toBe(receipt);
+})
 
 
 });
